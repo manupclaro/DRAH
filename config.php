e906869e71e163cf6858b3df711f2a29bde05ed4
manupclaro <manupclaro@gmail.com>
2026-07-07 10:00:08 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,15 @@ define('BD_USER', 'root');
 define('BD_PASS', '');
 define('BD_NAME', 'drah');
 
-$conexao = mysqli_connect('localhost', BD_USER, BD_PASS);
+$conexao = new mysqli(
+    'localhost',
+    BD_USER,
+    BD_PASS,
+    BD_NAME
+);
 
-if (!$conexao) {
-    die("Erro na conexão: " . mysqli_connect_error());
+if ($conexao->connect_error) {
+    die("Erro na conexão: " . $conexao->connect_error);
 }
 
 mysqli_select_db($conexao, BD_NAME);
